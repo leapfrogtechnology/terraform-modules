a03df5b4bd9b2a66b67d3b1569589565b82e1ce3
Sparsha <sparshadotel@gmail.com>
2019-06-18 14:32:40 +0545

    DR-14 Terraform Module For VPC - Same Subnet Conflict for Private and Public Fix

--- vpc/main.tf
+++ vpc/main.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "private" {
 # Create a Public Subnet
 resource "aws_subnet" "public" {
   count = var.az_count
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
   vpc_id = aws_vpc.main.id
   availability_zone = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
