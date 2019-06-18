04a2bf2a5e03b6e5c41a629c8d081e26f428cda1
Rabin Gaire <rabingaire20@gmail.com>
2019-06-18 14:48:37 +0545

    DR-15 Add types to all input parameters

--- alb/variables.tf
+++ alb/variables.tf
@@ -2,25 +2,32 @@ variable "public_subnets" {
   type = "list"
 }
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  type = "string"
+}
 
 variable "health_check_path" {
+  type = "string"
   default = "/"
 }
 
 variable "alb_security_group_name" {
+  type = "string"
   default = "cb-load-balancer-security-group"
 }
 
 variable "alb_security_group_description" {
+  type = "string"
   default = "controls access to the ALB"
 }
 
 variable "alb_name" {
+  type = "string"
   default = "cb-load-balancer"
 }
 
 variable "alb_target_name" {
+  type = "string"
   default = "cb-target-group"
 }
 
@@ -32,4 +39,6 @@ variable "tags" {
   }
 }
 
-variable "aws_acm_certificate_arn" {}
+variable "aws_acm_certificate_arn" {
+  type = "string"
+}
