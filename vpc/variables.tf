cf2b9606d7e3b0744de86465f29c357839ab3496
Sparsha <sparshadotel@gmail.com>
2019-06-18 13:08:40 +0545

    DR-14 Terraform Module For VPC completed

---
+++ vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "az_count" {
+  type = "string"
+  default = "2"
+}
+
+variable "tags" {
+  type = "map"
+  default = {
+    Name = "Default"
+    Project = "Default"
+  }
+}
+
+variable "cidr_block" {
+  default = "172.17.0.0/16"
+}
