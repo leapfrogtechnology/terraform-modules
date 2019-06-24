5c5925af57cf161245701787df27ec51dde3b83d
Rabin Gaire <rabingaire20@gmail.com>
2019-06-24 12:49:59 +0545

    Add lambda module

---
+++ lambda/variables.tf
@@ -0,0 +1,29 @@
+variable "lambda_file_name" {
+  type = "string"
+}
+
+variable "lambda_function_name" {
+  type = "string"
+}
+
+variable "lambda_handler" {
+  type = "string"
+  default = "exports.test"
+}
+
+variable "lambda_runtime" {
+  type = "string"
+  default = "nodejs8.10"
+}
+
+variable "lambda_variables" {
+  type = "map"
+}
+
+variable "tags" {
+  type = "map"
+  default = {
+    Name = "lambda"
+    Project = "test"
+  }
+}
