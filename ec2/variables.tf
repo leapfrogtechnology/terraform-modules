39844ad787ec55ea34933a0275b1e9ebfc53539a
Sparsha <sparshadotel@gmail.com>
2019-11-22 12:02:23 +0545

    EC2 module for terraform

---
+++ ec2/variables.tf
@@ -0,0 +1,67 @@
+variable "ec2_security_group_name" {
+  type = "string"
+}
+
+variable "ec2_security_group_description" {
+  type = "string"
+  default = "EC2 Security Group Managed By Terraform"
+}
+
+variable "vpc_id" {
+  type = "string"
+}
+
+variable "ec2_instance_type" {
+  type = "string"
+  default = "t2.micro"
+}
+
+variable "public_subnet_id" {
+  type = "string"
+}
+
+variable "ec2_key_name" {
+  type = "string"
+}
+
+variable "ec2_cidr_blocks" {
+  type = "list"
+  default = [
+    "202.70.67.113/32",
+    "182.93.83.51/32",
+    "202.166.206.8/32",
+    "202.79.34.78/32",
+  ]
+}
+
+variable "image_filter_values" {
+  type = "list"
+  default = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+}
+
+variable "virtualization_filter_values" {
+  type = "list"
+  default = ["hvm"]
+}
+
+variable "image_owners" {
+  type = "list"
+  default = ["099720109477"]
+}
+
+variable "tags" {
+  type = "map"
+  default = {
+    Name = "Traktivity"
+    Project = "Traktivity"
+  }
+}
+
+variable "ec2_instnace_name" {
+  type = "string"
+}
+
+variable "ec2_volume_size" {
+  type = "string"
+  default = 30
+}
